b6854d12dc43e83bfccd93104d1afb0098900411
shikachii <shikachii095@gmail.com>
2020-02-16 02:11:10 +0900

    [modify] 生成文字の変更

--- content.js
+++ content.js
@@ -3,6 +3,9 @@ const WIDTH = 5, HEIGHT = 5
 function putPattern(p, ctx, x, y){
 	if(p === 1){ ctx.fillStyle = "rgb(0, 0, 0)" }
 	else if(p === 2) { ctx.fillStyle = "rgb(100, 100, 100)" }
+	else if(p === 3) { ctx.fillStyle = "rgb(100,   0,   0)" }
+	else if(p === 4) { ctx.fillStyle = "rgb(  0, 100,   0)" }
+	else if(p === 5) { ctx.fillStyle = "rgb(  0,   0, 100)" }
 	else { ctx.fillStyle = "rgb(255, 255, 255)" }
 	ctx.fillRect(x*WIDTH, y*HEIGHT, WIDTH, HEIGHT)
 }
@@ -13,6 +16,7 @@ function reflectPattern(code, ctx){
 		for(let j = 0; j < code.size; ++j){
 			if(code.reserved[i][j] === false) putPattern(2, ctx, i, j)
 			else putPattern(arr[i][j], ctx, i, j)
+			// if(i === code.size-1) console.log("i:"+i+",j:"+j+",ar:"+arr[i][j])
 		}
 	}
 }
@@ -20,10 +24,10 @@ function reflectPattern(code, ctx){
 chrome.extension.onRequest.addListener(() => {
 	const url = window.location.href
 	const qr = new QR(
-			"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-			//url
+			//"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+			url
 			//"1234567890abcdefghijklmnopqrstuvwxyz"
-			, "M")
+			, "H")
 	
 	// 描画用canvasの初期化
 	let canvas = document.createElement("canvas")
